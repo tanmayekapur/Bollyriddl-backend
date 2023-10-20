325ce507bdac0638072c7d7a05e2bb85ee2ebfd8
Girish Mahale <girishmahale786@gmail.com>
2023-10-21 03:04:04 +0530

    added movie db schema

---
+++ movie/models.py
@@ -0,0 +1,124 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField("Name", unique=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def validate_unique(self, exclude):
+        self.name = self.name.title()
+        return super().validate_unique(exclude)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        super().save(*args, **kwargs)
+
+
+class Cast(models.Model):
+    name = models.CharField("Name", unique=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def validate_unique(self, exclude):
+        self.name = self.name.title()
+        return super().validate_unique(exclude)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        super().save(*args, **kwargs)
+
+
+class Writer(models.Model):
+    name = models.CharField("Name", unique=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def validate_unique(self, exclude):
+        self.name = self.name.title()
+        return super().validate_unique(exclude)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        super().save(*args, **kwargs)
+
+
+class Director(models.Model):
+    name = models.CharField("Name", unique=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def validate_unique(self, exclude):
+        self.name = self.name.title()
+        return super().validate_unique(exclude)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        super().save(*args, **kwargs)
+
+
+class MusicDirector(models.Model):
+    name = models.CharField("Name", unique=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def validate_unique(self, exclude):
+        self.name = self.name.title()
+        return super().validate_unique(exclude)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        super().save(*args, **kwargs)
+
+
+class ProductionHouse(models.Model):
+    name = models.CharField("Name", unique=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def validate_unique(self, exclude):
+        self.name = self.name.title()
+        return super().validate_unique(exclude)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        super().save(*args, **kwargs)
+
+
+class Movie(models.Model):
+    name = models.CharField("Name", unique=True, max_length=255)
+    imdb_id = models.CharField("IMDB ID", unique=True, max_length=255)
+    year = models.IntegerField("Year of Release", default=0)
+    genres = models.ManyToManyField(Genre, verbose_name="Genres")
+    cast = models.ManyToManyField(Cast, verbose_name="Cast")
+    writers = models.ManyToManyField(Writer, verbose_name="Writers")
+    director = models.ForeignKey(
+        Director, verbose_name="Director", on_delete=models.CASCADE
+    )
+    music_directors = models.ManyToManyField(
+        MusicDirector, verbose_name="Music Directors"
+    )
+    production_house = models.ForeignKey(
+        ProductionHouse, verbose_name="Production House", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
+    def validate_unique(self, exclude):
+        self.name = self.name.title()
+        self.imdb_id = self.imdb_id.lower()
+        return super().validate_unique(exclude)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.title()
+        self.imdb_id = self.imdb_id.lower()
+        super().save(*args, **kwargs)
