065c7da337e139f09147039fede2d8d34f0787b6
Girish Mahale <girishmahale786@gmail.com>
2024-03-16 16:21:01 +0530

    added delete permission in useractivity

--- movie/admin.py
+++ movie/admin.py
@@ -207,9 +207,6 @@ class UserActivityAdmin(AnalyticsMixin, ImportExportMixin, admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
     def winner_display(self, obj):
         return obj.winner
 
